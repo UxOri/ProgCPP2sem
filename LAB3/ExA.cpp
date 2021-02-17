7fa28ca6a021a74c14a2bd3c5a80e6ed06fad70c
UxOri <72220533+UxOri@users.noreply.github.com>
2021-02-17 17:47:34 +0300

    Start

---
+++ LAB3/ExA.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+float mean (const int *data, const int size);
+
+int main() {
+    int N;
+    cin >> N;
+    int data[N];
+    for (int i = 0; i < N; i++) cin >> data[i];
+    float m = mean (data, N);
+
+    int S = 0;
+    for (int i = 0; i < N; i++) {
+        if (data[i] > m) S += data[i];
+    }
+    cout << S << endl;
+
+    return 0;
+}
+
+float mean (const int *data, const int size) {
+    int S = 0;
+    for (int i = 0; i < size; i++) S += data[i];
+    float M = float(S) / size;
+    return M;
+
+}
