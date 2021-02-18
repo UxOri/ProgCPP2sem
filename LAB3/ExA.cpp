90346459ee24c22773c6b1b2e48820cac3093c21
Sergei <reva.sk@phystech.edu>
2021-02-18 10:31:17 +0300

    ExA

--- LAB3/ExA.cpp
+++ LAB3/ExA.cpp
@@ -4,12 +4,13 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-float mean (const int *data, const int size);
+float mean (const int* data, const int size);
 
 int main() {
     int N;
     cin >> N;
-    int data[N];
+    //int* data = new int[N];
+    int data[1000];
     for (int i = 0; i < N; i++) cin >> data[i];
     float m = mean (data, N);
 
@@ -17,15 +18,16 @@ int main() {
     for (int i = 0; i < N; i++) {
         if (data[i] > m) S += data[i];
     }
-    cout << S << endl;
+    cout << S;
+    delete []data;
 
     return 0;
 }
 
-float mean (const int *data, const int size) {
+float mean (const int* data, const int size) {
     int S = 0;
     for (int i = 0; i < size; i++) S += data[i];
-    float M = float(S) / size;
+    float M = float(S / size);
     return M;
 
 }
